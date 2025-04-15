4ca200cbd4bc28bdc18940d97c8a8806ea89b147
GitusesoftwareJ <itnaiubmail@gmail.com>
2025-04-16 01:45:10 +0600

    ClubClientHtmlForm.php

--- ClubClientHtmlForm.php
+++ ClubClientHtmlForm.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Club Member Registration</title>
+    <link rel="stylesheet" href="client.css"> 
 </head>
 <body>
     <h2>Club Member Registration</h2>
@@ -9,16 +10,16 @@
         <table border="1">
             <tr>
                 <td>Full Name:</td>
-                <td><input type="text" name="full_name" required></td>
+                <td><input type="text" name="full_name"></td>
             </tr>
             <tr>
                 <td>Email Address:</td>
-                <td><input type="email" name="email" required></td>
+                <td><input type="email" name="email"></td>
             </tr>
             <tr>
                 <td>Club Interest:</td>
                 <td>
-                    <select name="interest" required>
+                    <select name="interest">
                         <option value="music">Music</option>
                         <option value="sports">Sports</option>
                         <option value="arts">Arts</option>
@@ -30,7 +31,7 @@
             <tr>
                 <td>Membership Type:</td>
                 <td>
-                    <select name="membership_type" required>
+                    <select name="membership_type">
                         <option value="normal">Normal</option>
                         <option value="vip">VIP</option>
                         <option value="premium">Premium</option>
@@ -40,7 +41,7 @@
             <tr>
                 <td>Membership Duration:</td>
                 <td>
-                    <select name="membership_duration" required>
+                    <select name="membership_duration">
                         <option value="1_year">1 Year</option>
                         <option value="2_years">2 Years</option>
                         <option value="3_years">3 Years</option>
@@ -50,7 +51,7 @@
             <tr>
                 <td>Payment Type:</td>
                 <td>
-                    <select name="payment_type" required>
+                    <select name="payment_type">
                         <option value="normal">Normal</option>
                         <option value="premium">Premium</option>
                     </select>
@@ -58,12 +59,15 @@
             </tr>
             <tr>
                 <td>Contact Number:</td>
-                <td><input type="text" name="contact" required></td>
+                <td><input type="text" name="contact"></td>
             </tr>
             <tr>
                 <td colspan="2"><input type="submit" value="Register"></td>
             </tr>
         </table>
     </form>
+
+    <!-- Link to external JavaScript file -->
+    <script src="clientscript.js"></script>
 </body>
 </html>
